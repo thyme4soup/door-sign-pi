e806e4872445fc8ef6b39577bd6cf3c45fa39bec
Michael Chenevey <mchenevey7@gmail.com>
2020-03-23 19:23:43 -0700

    ya

--- fauxmo.py
+++ fauxmo.py
@@ -320,8 +320,6 @@ class upnp_broadcast_responder(object):
         if data:
             data = str(data)
             if data.find('M-SEARCH') >= 0:
-                logging.debug(data)
-                logging.debug(sender)
                 logging.debug(data.find('M-SEARCH'))
                 if data.find('upnp:rootdevice') != -1:
                     logging.debug("Someone's looking for us! Responding...")
